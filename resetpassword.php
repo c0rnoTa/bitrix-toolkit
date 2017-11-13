a9b80bfad6c59c1a5ad47aaee19131130be2a3f1
c0rnoTa <c0rnoTa@gmail.com>
2017-11-13 14:23:18 +0300

    reset password added

---
+++ resetpassword.php
@@ -0,0 +1,5 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT']."/bitrix/header.php");
+echo $USER->Update(1,array("PASSWORD"=>'Bitrix*123456'));
+echo $USER->LAST_ERROR;
+require($_SERVER['DOCUMENT_ROOT']."/bitrix/footer.php");
